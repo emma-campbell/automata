925231177a8a2d596569c6635e2aa7ef7516957f
ecampb10@u.rochester.edu <>
2020-04-09 18:08:12 -0400

    New set structure

--- include/set.h
+++ include/set.h
@@ -1,22 +1,4 @@
 #ifndef _SET_h
 #define _SET_h
 
-#include <common.h>
-#include <rbtree.h>
-
-struct set_t {
-    RBTREE data;
-    int size;
-};
-
-typedef set_t *Set;
-
-extern Set set_create(cmp_func_t cmp_func);
-extern void set_destroy(Set s);
-extern void set_insert(Set s, void *data);
-extern void set_remove(Set s, void *data);
-
-extern Set set_join(Set s1, Set s2);
-extern Set set_union(Set s1, Set s2);
-
 #endif //_SET_h
