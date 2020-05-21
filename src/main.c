12a8bc03d64b65236b7f44d7cd982e5dabe6a9ef
ecampb10@u.rochester.edu <>
2020-05-21 17:15:40 -0400

    Finished NFA->DFA converter

--- src/main.c
+++ src/main.c
@@ -9,11 +9,11 @@ DFA make_first_dfa()
 {
 	/**
 	 * Exactly the string "csc173"
-	 */ 
+	 */
 	DFA dfa = NULL;
 
 	dfa = new_DFA(7);
-	
+
 	DFA_set_transition(dfa, 0, 'c', 1);
 	DFA_set_transition(dfa, 1, 's', 2);
 	DFA_set_transition(dfa, 2, 'c', 3);
@@ -42,15 +42,14 @@ DFA make_second_dfa()
 	return dfa;
 }
 
-
 DFA make_third_dfa()
 {
 	/**
 	 * Accepts an even number of 1's
 	 */
-	DFA dfa = NULL; 
+	DFA dfa = NULL;
 	dfa = new_DFA(2);
-	
+
 	DFA_set_transition(dfa, 0, '0', 1);
 	DFA_set_transition(dfa, 1, '1', 1);
 	DFA_set_transition(dfa, 1, '0', 0);
@@ -59,7 +58,6 @@ DFA make_third_dfa()
 	DFA_set_accepting(dfa, 0);
 	return dfa;
 }
-
 
 DFA make_fourth_dfa()
 {
@@ -86,14 +84,14 @@ NFA make_first_nfa()
 {
 	NFA nfa = NULL;
 	nfa = new_NFA(5);
-	
+
 	NFA_add_transition(nfa, 0, 'c', 0);
-    	NFA_add_transition(nfa, 0, 'c', 1);
-        NFA_add_transition_all(nfa, 0, 0);
-    	NFA_add_transition(nfa, 1, 'o', 2);
-    	NFA_add_transition(nfa, 2, 'd', 3);
-    	NFA_add_transition(nfa, 3, 'e', 4);
-    
+	NFA_add_transition(nfa, 0, 'c', 1);
+	NFA_add_transition_all(nfa, 0, 0);
+	NFA_add_transition(nfa, 1, 'o', 2);
+	NFA_add_transition(nfa, 2, 'd', 3);
+	NFA_add_transition(nfa, 3, 'e', 4);
+
 	NFA_set_accepting(nfa, 4);
 	return nfa;
 }
@@ -103,56 +101,55 @@ NFA make_second_nfa()
 
 	NFA nfa = NULL;
 	nfa = new_NFA(5);
-	
+
 	NFA_add_transition(nfa, 0, 'c', 0);
-    	NFA_add_transition(nfa, 0, 'c', 1);
-        NFA_add_transition_all(nfa, 0, 0);
-    	NFA_add_transition(nfa, 1, 'o', 2);
-    	NFA_add_transition(nfa, 2, 'd', 3);
-    	NFA_add_transition(nfa, 3, 'e', 4);
-   	NFA_add_transition_all(nfa, 4, 4);
+	NFA_add_transition(nfa, 0, 'c', 1);
+	NFA_add_transition_all(nfa, 0, 0);
+	NFA_add_transition(nfa, 1, 'o', 2);
+	NFA_add_transition(nfa, 2, 'd', 3);
+	NFA_add_transition(nfa, 3, 'e', 4);
+	NFA_add_transition_all(nfa, 4, 4);
 
 	NFA_set_accepting(nfa, 4);
 	return nfa;
-
 }
 
 NFA make_third_nfa()
 {
 	NFA nfa = new_NFA(20);
-	NFA_add_transition_all(nfa, 0,0);
-	NFA_add_transition_all(nfa, 1,1);
-  	NFA_add_transition_all(nfa, 2,2);
-  	NFA_add_transition_all(nfa, 3,3);
-  	NFA_add_transition_all(nfa, 4,4);
-  	NFA_add_transition_all(nfa, 5,5);
-  	NFA_add_transition_all(nfa, 6,6);
-  	NFA_add_transition_all(nfa, 7,7);
-  	NFA_add_transition_all(nfa, 8,8);
-  	NFA_add_transition_all(nfa, 9,9);
-  	NFA_add_transition_all(nfa, 10,10);
-  
-	NFA_add_transition(nfa,0, 'a', 1);
-  
-	NFA_add_transition(nfa,1, 'a', 10);
-  
-	NFA_add_transition(nfa,0, 'g', 2);
-	NFA_add_transition(nfa,2, 'g', 10);
-	NFA_add_transition(nfa,0, 'h', 3);
-	NFA_add_transition(nfa,3, 'h', 10);
-	NFA_add_transition(nfa,0, 'i', 4);
-	NFA_add_transition(nfa,4,'i', 10);
-	NFA_add_transition(nfa,0, 'o', 5);
-	NFA_add_transition(nfa,5, 'o', 10);
-	NFA_add_transition(nfa,0, 's', 6);
-	NFA_add_transition(nfa,6, 's', 10);
-	NFA_add_transition(nfa,0, 't', 7);
-	NFA_add_transition(nfa,7, 't', 10);
-	NFA_add_transition(nfa,0, 'n', 8);
-	NFA_add_transition(nfa,8, 'n', 9);
-	NFA_add_transition(nfa,9, 'n', 10);
-	NFA_add_transition(nfa,0, 'w', 11);
-	NFA_add_transition(nfa,11, 'w', 10);
+	NFA_add_transition_all(nfa, 0, 0);
+	NFA_add_transition_all(nfa, 1, 1);
+	NFA_add_transition_all(nfa, 2, 2);
+	NFA_add_transition_all(nfa, 3, 3);
+	NFA_add_transition_all(nfa, 4, 4);
+	NFA_add_transition_all(nfa, 5, 5);
+	NFA_add_transition_all(nfa, 6, 6);
+	NFA_add_transition_all(nfa, 7, 7);
+	NFA_add_transition_all(nfa, 8, 8);
+	NFA_add_transition_all(nfa, 9, 9);
+	NFA_add_transition_all(nfa, 10, 10);
+
+	NFA_add_transition(nfa, 0, 'a', 1);
+
+	NFA_add_transition(nfa, 1, 'a', 10);
+
+	NFA_add_transition(nfa, 0, 'g', 2);
+	NFA_add_transition(nfa, 2, 'g', 10);
+	NFA_add_transition(nfa, 0, 'h', 3);
+	NFA_add_transition(nfa, 3, 'h', 10);
+	NFA_add_transition(nfa, 0, 'i', 4);
+	NFA_add_transition(nfa, 4, 'i', 10);
+	NFA_add_transition(nfa, 0, 'o', 5);
+	NFA_add_transition(nfa, 5, 'o', 10);
+	NFA_add_transition(nfa, 0, 's', 6);
+	NFA_add_transition(nfa, 6, 's', 10);
+	NFA_add_transition(nfa, 0, 't', 7);
+	NFA_add_transition(nfa, 7, 't', 10);
+	NFA_add_transition(nfa, 0, 'n', 8);
+	NFA_add_transition(nfa, 8, 'n', 9);
+	NFA_add_transition(nfa, 9, 'n', 10);
+	NFA_add_transition(nfa, 0, 'w', 11);
+	NFA_add_transition(nfa, 11, 'w', 10);
 	NFA_set_accepting(nfa, 10);
 	return nfa;
 }
@@ -177,7 +174,7 @@ void test_dfa(DFA d)
 		{
 			printf("Result for \"%s\": true\n", input);
 		}
-		else 
+		else
 		{
 			printf("Result for \"%s\": false\n", input);
 		}
@@ -207,7 +204,7 @@ void test_nfa(NFA n)
 		{
 			printf("Result for \"%s\": true\n", input);
 		}
-		else 
+		else
 		{
 			printf("Result for \"%s\": false\n", input);
 		}
@@ -216,7 +213,6 @@ void test_nfa(NFA n)
 
 	NFA_free(n);
 }
-
 
 void test_washington(NFA n)
 {
@@ -238,7 +234,7 @@ void test_washington(NFA n)
 		{
 			printf("\"%s\" is not a partial anagram of \"washington\"\n", input);
 		}
-		else 
+		else
 		{
 			printf("\"%s\" is a partial anagram of \"washington\"\n", input);
 		}
@@ -247,6 +243,7 @@ void test_washington(NFA n)
 
 	NFA_free(n);
 }
+
 int main(void)
 {
 	DFA d1, d2, d3, d4;
@@ -272,14 +269,13 @@ int main(void)
 
 	d4 = make_fourth_dfa();
 	test_dfa(d4);
-	
 
 	printf("\n\nTesting NFA that recognizes strings ending in \"code\"...\n");
 	n1 = make_first_nfa();
 	test_nfa(n1);
-	
+
 	printf("\n\nTesting NFA that recognizes strings containing \"code\"...\n");
-	
+
 	n2 = make_second_nfa();
 	test_nfa(n2);
 
@@ -287,4 +283,21 @@ int main(void)
 	n3 = make_third_nfa();
 	test_washington(n3);
 
-}	
+	printf("\n\nNow converting NFA that recognizes strings ending in \"code\"...\n");
+	NFA code = make_first_nfa();
+	printf("The NFA has %d states, ", NFA_get_size(code));
+	DFA converted = NFA_to_DFA(code);
+	printf("It has %d states once converted.", DFA_get_size(converted));
+
+	printf("\n\nNow converting NFA that recognizes strings containing \"code\"...\n");
+	NFA end_in_code = make_second_nfa();
+	printf("The NFA has %d states, ", NFA_get_size(end_in_code));
+	DFA converted_code = NFA_to_DFA(end_in_code);
+	printf("It has %d states once converted.", DFA_get_size(converted_code));
+
+	printf("\n\nNow converting NFA that recognizes partial anagrams of \"washington\"...\n");
+	NFA washington = make_third_nfa();
+	printf("The NFA has %d states, ", NFA_get_size(washington));
+	DFA washingtonDFA = NFA_to_DFA(washington);
+	printf("It has %d states once converted.", DFA_get_size(washingtonDFA));
+}
---
+++ src/transformer.c
@@ -0,0 +1,185 @@
+
+// .-. . . .-. .   . . .-. .-. .-. 
+//  |  |\| |   |   | | |  )|-  `-. 
+// `-' ' ` `-' `-' `-' `-' `-' `-' 
+#include <nfa.h>
+#include <dfa.h>
+
+#include <IntHashSet.h>
+#include <LinkedList.h>
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// .-. .-. .-. .-. . . .-. 
+// |  )|-  |-   |  |\| |-  
+// `-' `-' '   `-' ' ` `-' 
+
+/**
+ * @brief Return whether the list contains the given set
+ * 
+ * @param list pointer to the list
+ * @param set pointer to the set
+ * @return true if the set is in the list
+ * @return false if the set is not in the list
+ */
+bool contains(LinkedList list, IntHashSet set)
+{
+    LinkedListIterator it = LinkedList_iterator(list);
+    
+    while (LinkedListIterator_hasNext(it)) 
+    {
+        if (IntHashSet_equals(LinkedListIterator_next(it), set))
+        {
+            free(it);
+            return true;
+        }
+    }
+ 
+    free(it);
+    return false;
+}
+
+/**
+ * @brief Insert the set into the list (Don't insert if already in the list)
+ * 
+ * @param list pointer to the list
+ * @param set pointer to the set
+ */
+void insert(LinkedList list, IntHashSet set)
+{
+    if (!LinkedList_contains(list, set))
+    {
+        LinkedList_add_at_end(list, (void *)set);
+    }
+}
+
+/**
+ * @brief Return the indexState of the given set within the list
+ * 
+ * @param list pointer to the list
+ * @param set pointer to the set
+ * @return int the indexState of the set within the list
+ */
+int index(LinkedList list, IntHashSet set)
+{
+    LinkedListIterator it = LinkedList_iterator(list);
+    int indexState = 0;
+
+    while (LinkedListIterator_hasNext(it))
+    {
+        if (IntHashSet_equals(LinkedListIterator_next(it), set))
+        {
+            return indexState;
+        }
+        indexState++;
+    }
+    return indexState;
+}
+
+/**
+ * @brief Return the size of the LinkedList
+ * 
+ * @param list pointer to the list
+ * @return int size of the list
+ */
+int size(LinkedList list)
+{
+    LinkedListIterator it = LinkedList_iterator(list);
+    int size = 0;
+
+    while (LinkedListIterator_hasNext(it))
+    {
+        LinkedListIterator_next(it);
+        size++;
+    }
+    return size;
+}
+
+/**
+ * @brief Convert the NFA into new DFA
+ * 
+ * @param nfa pointer to the NFA
+ * @return DFA Pointer to the converted DFA
+ */
+DFA NFA_to_DFA(NFA nfa) 
+{
+
+    // Theoretically, the maximum number of states in a DFA after
+    // conversion from a NFA should be numNFAStates^2
+    DFA dfa = new_DFA((int)pow(2, NFA_get_size(nfa)));
+
+    LinkedList finalStates = new_LinkedList();
+    LinkedList sets = new_LinkedList();
+
+    // pointers to the states we are creating (set of states)
+    IntHashSet state = new_IntHashSet(DFA_get_size(dfa));
+    IntHashSet nextState = new_IntHashSet(DFA_get_size(dfa));
+    IntHashSet_insert(state, 0);
+
+    LinkedList_add_at_front(finalStates, state);
+    LinkedList_add_at_front(sets, state);
+
+    int currState = 0;
+    int indexState = 0;
+    int dst = 0;
+
+    IntHashSet tmp = NULL;
+
+    while (!LinkedList_isEmpty(sets))
+    {
+        IntHashSet unionSet = new_IntHashSet(128);
+
+        tmp = LinkedList_pop(sets);
+
+        state = tmp;
+        IntHashSetIterator it = IntHashSet_iterator(state);
+
+        while (IntHashSetIterator_hasNext(it))
+        {
+            currState = IntHashSetIterator_next(it);
+            for (int i = 0; i < 128; i++) {
+                nextState = NFA_get_transitions(nfa, currState, (char)i);
+                
+                if (!IntHashSet_equals(unionSet, nextState))
+                {
+                    IntHashSet_union(unionSet, nextState);
+                }
+            }
+        }
+
+        for (int i = 0; i < 128; i++) 
+        {
+            if (!contains(finalStates, unionSet))
+            {
+                LinkedList_add_at_end(finalStates, unionSet);
+                LinkedList_add_at_end(sets, unionSet);
+            }
+
+            indexState = index(finalStates, state);
+            dst = index(finalStates, unionSet);
+
+            DFA_set_transition(dfa, indexState, (char)i, dst);
+        }
+    }
+
+    LinkedListIterator it_accepting = LinkedList_iterator(finalStates);
+
+    while (LinkedListIterator_hasNext(it_accepting))
+    {
+        IntHashSet currSet = LinkedListIterator_next(it_accepting);
+        IntHashSetIterator itSet = IntHashSet_iterator(currSet);
+
+        while (IntHashSetIterator_hasNext(itSet))
+        {
+            int st = IntHashSetIterator_next(itSet);
+            if (NFA_get_accepting(nfa, st))
+            {
+                DFA_set_accepting(dfa, index(finalStates, currSet));
+            }
+        }
+    }
+
+    return dfa;
+}
